f5f3417305dfd3e4cba8d0f28ba4ff9953a0ba02
asingh <87902414+devopslearner001@users.noreply.github.com>
2026-04-28 02:44:00 +0530

    Update resourceGroup.tf

--- Azure_ResourceGroup/resourceGroup.tf
+++ Azure_ResourceGroup/resourceGroup.tf
@@ -13,18 +13,17 @@ terraform {
 ## Configure the Azure provider ##
 
 provider "azurerm" {
-  features {}   # Required block (even if empty) to enable Azure provider features
+  features {}                       # Required block (even if empty) to enable Azure provider features
 }
 
 
 ## Create a Resource Group in Azure ##
 
 resource "azurerm_resource_group" "rg" {
-  name       = "asg-rg-old"      # Name of the Resource Group (must be unique within your subscription)
-  location   = "West Europe"     # Azure region where resources will be deployed
-  managed_by = "Terraform"       # Optional field to indicate this resource is managed by Terraform
-  tags = {                       # Key-value metadata for organization and cost tracking
-    environment = "prod"         # Defines environment (e.g., dev, staging, prod)
-    owner       = "atul_singh"   # Owner or responsible person/team
+  name       = "asg-rg-old"        # Name of the Resource Group (must be unique within your subscription)
+  location   = "West Europe"       # Azure region where resources will be deployed
+  managed_by = "Terraform"         # Optional field to indicate this resource is managed by Terraform
+  tags = {                         # Key-value metadata for organization and cost tracking
+    environment = "prod"           # Defines environment (e.g., dev, staging, prod)
   }
 }
